a33c4cafb9aec702a4fefc0e27e2eeb757b67e3d
mor19034 <68123745+mor19034@users.noreply.github.com>
2021-08-23 19:39:58 -0600

    Ya están los repositorios

---
+++ Proyecto1.X/Master_proyecto1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Master_proyecto1.c
+ * Author: pablo
+ *
+ * Created on 23 de agosto de 2021, 19:39
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
